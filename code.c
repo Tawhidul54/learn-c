c58f367f3fee8b98b206b38a89bb8ccc87cbbe90
tawhid <tawhidul8643@gmail.com>
2025-02-05 12:37:02 +0600

    i'm learning c

---
+++ code.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "custom.h"
+
+int main () {
+
+   printf("age=%d\nid=%d",age,id );
+   return 0;
+
+}
---
+++ custom.h
@@ -0,0 +1,3 @@
+int  age = 33;
+int id = 456;
+float  number = 8.90;
