eb6f3934897d58525865c5156009589e4b9db2e1
tawhid <tawhidul8643@gmail.com>
2025-02-06 13:13:51 +0600

    c and new learning javascript

---
+++ app.js
@@ -0,0 +1,11 @@
+let a = 78.9;
+let b = 67.4;
+let c = a * b ;
+
+let student = [
+    {Name: "Tawhid", age:16},
+    {Name: "Tanvir", age:16},
+    {Name: "Tahmid", age:16},
+    {Name: "Tawhid", age:16},    
+];
+console.table(student);
